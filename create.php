f5e19d4b8ff9a18f1d5b6735c2a9fe45a121584b
Tihitza <d292003@edu.curio.nl>
2025-03-12 16:43:26 +0100

    Update create.php
    Added the formgroup div to the form

--- create.php
+++ create.php
@@ -1,17 +1,26 @@
 <!doctype html>
 <html lang="nl">
     <form action="../controllers/taskcontroller.php" method="POST">
-        <label for="title">Titel</label><br>
-        <input type="text" id="title" name="title"><br>
+        <div class="formgroup">
+            <label for="title">Titel</label><br>
+        </div>
+        <div class="formgroup">
+            <input type="text" id="title" name="title"><br>
+        </div>
+        <div class="formgroup">
         <label for="department">Afdeling:</label><br>
-        <select name="department" id="departments">
-            <option value="IT">IT</option>
-            <option value="HR">Human Resources</option>
-            <option value="Reception">Reception</option>
-            <option value="Skibidi">Skibidi department</option>
-            
-        </select>
-        <label for="task">Taak:</label><br>
-        <textarea id="task" name="task" rows="4" cols="50"></textarea>
+        </div>
+        <div class="formgroup">
+            <select name="department" id="departments">
+                <option value="IT">IT</option>
+                <option value="HR">Human Resources</option>
+                <option value="Reception">Reception</option>
+                <option value="Skibidi">Skibidi department</option>
+            </select>
+        </div>
+        <div class="formgroup">
+            <label for="task">Taak:</label><br>
+            <textarea id="task" name="task" rows="4" cols="50"></textarea>
+        </div>
     </form>
 </html>
