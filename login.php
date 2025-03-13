3390f451f97cff75071ba408d811d974f8f8d2f5
HyperNeutron <nigel.koremans@gmail.com>
2025-03-13 11:40:17 +0100

    created login form

--- login.php
+++ login.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<?php require_once "components/head.php"?>
+<?php
+session_start();
+require_once "components/head.php";
+?>
 
 <body>
-    
+<form action="controllers/logincontroller.php" method="post">
+        <input type="hidden" name="action" value="login">
+        <div class="formgroup">
+            <label for="username">gebruikersnaam</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div class="formgroup">
+            <label for="email">email</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div class="formgroup">
+            <label for="wachtworod">wachtwoord</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <input type="submit" value="sturen">
+    </form>
 </body>
 </html>
