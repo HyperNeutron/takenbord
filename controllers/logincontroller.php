234e0c862dfe26e3df69180ac22b38c374b75495
HyperNeutron <nigel.koremans@gmail.com>
2025-03-17 09:17:40 +0100

    added login functionality

--- controllers/logincontroller.php
+++ controllers/logincontroller.php
@@ -20,3 +20,29 @@ if ($_POST["action"] == "register") {
         ":email" => $email,
     ]);
 }
+elseif ($_POST["action"] == "login"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $query = "SELECT * FROM users WHERE username = :username"; 
+
+    $statement = $conn->prepare($query); 
+    $statement->execute([
+        ":username" => $username,
+    ]);
+
+    if ($statement->rowCount() < 1) {
+        die("Error: account bestaat niet");
+    }
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+    
+    if (!password_verify($password, $user['password'])) {
+    
+        die("Error: wachtwoord niet juist!");
+    }
+
+    $_SESSION["user_id"] = $user["id"];
+
+    header("location: ../index.php?msg=logged in");
+}
