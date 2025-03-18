ea1f86cd16ea1f4902b773dbf72bfffd221a6104
HyperNeutron <nigel.koremans@gmail.com>
2025-03-18 09:05:08 +0100

    added container div

--- login.php
+++ login.php
@@ -9,25 +9,26 @@
 
 <body>
     <?php require_once "components/header.php" ?>
-
-    <a href="register.php">registreren</a>
-    <?php if (isset($_GET["msg"])): ?>
-        <div class="error">
-            <?php echo $_GET["msg"]; ?>
-        </div>
-    <?php endif ?>
-    <form action="controllers/logincontroller.php" method="post">
-        <input type="hidden" name="action" value="login">
-        <div class="formgroup">
-            <label for="username">gebruikersnaam</label>
-            <input type="text" name="username" id="username">
-        </div>
-        <div class="formgroup">
-            <label for="password">wachtwoord</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <input type="submit" value="sturen">
-    </form>
+    <div class="container">
+        <a href="register.php">registreren</a>
+        <?php if (isset($_GET["msg"])): ?>
+            <div class="error">
+                <?php echo $_GET["msg"]; ?>
+            </div>
+        <?php endif ?>
+        <form action="controllers/logincontroller.php" method="post">
+            <input type="hidden" name="action" value="login">
+            <div class="formgroup">
+                <label for="username">gebruikersnaam</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div class="formgroup">
+                <label for="password">wachtwoord</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <input type="submit" value="sturen">
+        </form>
+    </div>
 </body>
 
 </html>
