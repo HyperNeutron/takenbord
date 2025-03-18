b2a9d0ae066f2d036f3d40b1b34151cdcace024b
HyperNeutron <nigel.koremans@gmail.com>
2025-03-18 09:01:31 +0100

    remove head tags

--- components/head.php
+++ components/head.php
@@ -1,7 +1,5 @@
 <?php require_once __DIR__ . "/../config/config.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo $base_url ?>/components/style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="<?php echo $base_url ?>/components/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
