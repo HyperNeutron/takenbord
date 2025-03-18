897fe1fbeccac54f50ecd5954b8e896243adee50
HyperNeutron <nigel.koremans@gmail.com>
2025-03-18 10:17:00 +0100

    changed head to use head.php

--- edit.php
+++ edit.php
@@ -25,8 +25,7 @@ if (!$task) {
 <html lang="nl">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "components/head.php" ?>
     <title>Taak Bewerken</title>
 </head>
 
