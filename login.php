179b15d0cdde5f9b4b1a8177211c7ddc72c4b958
HyperNeutron <nigel.koremans@gmail.com>
2025-03-18 08:34:07 +0100

    removed email field

--- login.php
+++ login.php
@@ -13,10 +13,6 @@
             <input type="text" name="username" id="username">
         </div>
         <div class="formgroup">
-            <label for="email">email</label>
-            <input type="text" name="email" id="email">
-        </div>
-        <div class="formgroup">
             <label for="password">wachtwoord</label>
             <input type="password" name="password" id="password">
         </div>
