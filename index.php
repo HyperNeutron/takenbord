a0fc2139743265a56295dbf17b21a952026fa8b3
HyperNeutron <nigel.koremans@gmail.com>
2025-03-18 10:15:10 +0100

    added container div

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="nl">
-<?php 
-session_start(); 
+<?php
+session_start();
 require_once 'config/conn.php';
 
 $query = "SELECT * FROM tasks ORDER BY id DESC";
@@ -14,49 +14,53 @@ $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
 <?php require_once "components/header.php" ?>
 
 <body>
-    <h1>Takenbord</h1>
-    <div>
-        <a href="login.php">Inloggen |</a>
-        <a href="create.php">Nieuwe Ticket</a>
-    </div>
+    <?php require_once "components/header.php" ?>
+    <div class="container">
+        <h1>Takenbord</h1>
+        <div>
+            <a href="login.php">Inloggen |</a>
+            <a href="create.php">Nieuwe Ticket</a>
+        </div>
 
-    <table border="1">
-        <tr>
-            <th>Titel</th>
-            <th>Afdeling</th>
-            <th>gebruiker</th>
-            <th>Prioriteit</th>
-            <th>Taak</th>
-            <th>Status</th>
-            <th>Acties</th>
-        </tr>
-        <?php if (!empty($tasks)): ?>
-            <?php foreach($tasks as $task): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($task['title']); ?></td>
-                    <td><?php echo htmlspecialchars($task['department']); ?></td>
-                    <td><?php echo htmlspecialchars($task['user']); ?></td>
-                    <td><?php echo htmlspecialchars($task['priority']); ?></td>
-                    <td><?php echo htmlspecialchars($task['description']); ?></td>
-                    <td><?php echo htmlspecialchars($task['status']); ?></td>
-                    <td>
-                        <a href="edit.php?id=<?php echo $task['id']; ?>">Verander Ticket</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        <?php else: ?>
+        <table border="1">
             <tr>
-                <td colspan="5">Geen taken gevonden.</td>
+                <th>Titel</th>
+                <th>Afdeling</th>
+                <th>gebruiker</th>
+                <th>Prioriteit</th>
+                <th>Taak</th>
+                <th>Status</th>
+                <th>Acties</th>
             </tr>
-        <?php endif; ?>
-    </table>
+            <?php if (!empty($tasks)): ?>
+                <?php foreach ($tasks as $task): ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($task['title']); ?></td>
+                        <td><?php echo htmlspecialchars($task['department']); ?></td>
+                        <td><?php echo htmlspecialchars($task['user']); ?></td>
+                        <td><?php echo htmlspecialchars($task['priority']); ?></td>
+                        <td><?php echo htmlspecialchars($task['description']); ?></td>
+                        <td><?php echo htmlspecialchars($task['status']); ?></td>
+                        <td>
+                            <a href="edit.php?id=<?php echo $task['id']; ?>">Verander Ticket</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="5">Geen taken gevonden.</td>
+                </tr>
+            <?php endif; ?>
+        </table>
 
-    <div>
-        <p>
-            <?php if (isset($_GET["msg"])) {
-                echo htmlspecialchars($_GET["msg"]);
-            } ?>
-        </p>
+        <div>
+            <p>
+                <?php if (isset($_GET["msg"])) {
+                    echo htmlspecialchars($_GET["msg"]);
+                } ?>
+            </p>
+        </div>
     </div>
 </body>
+
 </html>
