c81a258516656968cf14a607424637a6250be9aa
HyperNeutron <nigel.koremans@gmail.com>
2025-03-19 13:29:50 +0100

    added missing /

--- components/header.php
+++ components/header.php
@@ -1,10 +1,10 @@
 <div>
     <nav>
-        <a href="<?php echo $base_url ?>index.php">Home</a>
+        <a href="<?php echo $base_url ?>/index.php">Home</a>
         <?php if(isset($_SESSION["user_id"])): ?>
-        <a href="<?php echo $base_url ?>logout.php">uitloggen</a>
+        <a href="<?php echo $base_url ?>/logout.php">uitloggen</a>
         <?php else: ?>
-        <a href="<?php echo $base_url ?>login.php">inloggen</a>
+        <a href="<?php echo $base_url ?>/login.php">inloggen</a>
         <?php endif ?>
     </nav>
 </div>
