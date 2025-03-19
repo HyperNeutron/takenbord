0c342db7f7a568411437d5c1573aec39384c7b1b
D311154 <d311154@edu.curio.nl>
2025-03-19 14:29:01 +0100

    takenbord en landing page properly gemaakt
    lees de titel. takenbord en landing page zijn nu andere paginas (takenbord link is placeholder)

--- index.php
+++ index.php
@@ -21,46 +21,7 @@ $tasks = $statement->fetchAll(PDO::FETCH_ASSOC);
         <h1>Takenbord</h1>
         <div>
             <a class="link" href="login/login.php">Inloggen</a> |
-            <a class="link" href="tasks/create.php">Nieuwe Ticket</a>
-        </div>
-
-        <table border="1">
-            <tr>
-                <th>Titel</th>
-                <th>Afdeling</th>
-                <th>gebruiker</th>
-                <th>Prioriteit</th>
-                <th>Taak</th>
-                <th>Status</th>
-                <th>Acties</th>
-            </tr>
-            <?php if (!empty($tasks)): ?>
-                <?php foreach ($tasks as $task): ?>
-                    <tr>
-                        <td><?php echo htmlspecialchars($task['title']); ?></td>
-                        <td><?php echo htmlspecialchars($task['department']); ?></td>
-                        <td><?php echo htmlspecialchars($task['user']); ?></td>
-                        <td><?php echo htmlspecialchars($task['priority']); ?></td>
-                        <td><?php echo htmlspecialchars($task['description']); ?></td>
-                        <td><?php echo htmlspecialchars($task['status']); ?></td>
-                        <td>
-                            <a href="tasks/edit.php?id=<?php echo $task['id']; ?>">Verander Ticket</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td colspan="5">Geen taken gevonden.</td>
-                </tr>
-            <?php endif; ?>
-        </table>
-
-        <div>
-            <p>
-                <?php if (isset($_GET["msg"])) {
-                    echo htmlspecialchars($_GET["msg"]);
-                } ?>
-            </p>
+            <a class="link" href="takenbord.php">takenbord</a>
         </div>
     </div>
 </body>
