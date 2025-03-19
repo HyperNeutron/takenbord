931eb8294ff4ee2dc5916601ba9fc478e6311ed9
HyperNeutron <nigel.koremans@gmail.com>
2025-03-19 13:31:00 +0100

    fixed link to login and logout

--- components/header.php
+++ components/header.php
@@ -2,9 +2,9 @@
     <nav>
         <a href="<?php echo $base_url ?>/index.php">Home</a>
         <?php if(isset($_SESSION["user_id"])): ?>
-        <a href="<?php echo $base_url ?>/logout.php">uitloggen</a>
+        <a href="<?php echo $base_url ?>login/logout.php">uitloggen</a>
         <?php else: ?>
-        <a href="<?php echo $base_url ?>/login.php">inloggen</a>
+        <a href="<?php echo $base_url ?>login/login.php">inloggen</a>
         <?php endif ?>
     </nav>
 </div>
