62ed9e1e2b68eccf203f57fd18c9c8e5df425942
HyperNeutron <nigel.koremans@gmail.com>
2025-04-01 16:08:26 +0200

    moved register link below error message(s)

--- login/login.php
+++ login/login.php
@@ -11,12 +11,12 @@
     <?php require_once "../components/header.php" ?>
     <div class="container">
         <h1>Inloggen</h1>
-        <a class="link" href="register.php">Nog geen account?</a>
         <?php if (isset($_GET["msg"])): ?>
             <div class="error">
                 <?php echo $_GET["msg"]; ?>
             </div>
         <?php endif ?>
+        <a class="link" href="register.php">Nog geen account?</a>
         <form action="../controllers/logincontroller.php" method="post" class="login">
             <div class="formField">
                 <input type="hidden" name="action" value="login">
